99bab29bba6b594e69648c6108542307a61c5460
CD MacBook <corydavis1989@gmail.com>
2021-12-12 18:51:48 -0500

    if statements for time block colors

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ let inputTextBtn = document.querySelector(".scheduleBtn");
 // time display
 function updateTime () {
     const now = moment();
-    const readableDisplay = now.format("dddd, MMMM Do YYYY, h:mm:ss a");
+    const readableDisplay = now.format("dddd, MMMM Do YYYY, hh:mm:ss A");
     clock.textContent = readableDisplay;
 }
 setInterval(updateTime, 1000);
@@ -13,11 +13,17 @@ updateTime();
 
 // updates Timeblock to the assigned color based on the hour
 function hourUpdater() {
-    var currentHour = moment().hours();
-    var blockHour = document.getElementById("#9");
+    var currentHour = moment().h;
+    var timeblockHour = document.getElementById("#9");
 
+    if (timeblockHour < currentHour) {
+    $(this).addClass("#past");
+    } else if (timeblockHour === currentHour) {
+    $(this).addClass("#present");   
+    } else if (timeblockHour > currentHour) {
+    $(this).addClass("#future");   
+    };
 };
-
 hourUpdater();
 
 // logs data to local storage
